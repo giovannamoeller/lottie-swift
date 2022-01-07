9fd515f3365408fc8e0722882c94beffbc5bcad5
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-06 22:16:17 -0300

    Initial Commit

---
+++ LottieTest/LottieTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieTestApp.swift
+//  LottieTest
+//
+//  Created by Giovanna Moeller on 06/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LottieTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
